088a9bdc298af315d7007a496c445564b4fc1b33
network-mapping-dev <ben.thomas@nmgroup.com>
2023-07-19 11:30:50 +0100

    readme

--- generate_report.bat
+++ generate_report.bat
@@ -2,6 +2,7 @@ set CONDAPATH=C:\ProgramData\Miniconda3
 set ENVNAME=mps-kpi
 if %ENVNAME%==base (set ENVPATH=%CONDAPATH%) else (set ENVPATH=~\.conda\envs\%ENVNAME%)
 call %CONDAPATH%\Scripts\activate.bat %ENVPATH%
+Rem add command line args here
 python main.py 
 call conda deactivate
-pause
+pause 
